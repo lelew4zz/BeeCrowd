dbc34bbcc4e0514d125c47b57997e3dc61daa319
lelew4zz <alessandrasouzadelima39@gmail.com>
2026-09-03 17:19:21 -0300

    1067

---
+++ 1067.cs
@@ -0,0 +1,22 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+      
+int X = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= X; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+
+
+    }
+
+}
