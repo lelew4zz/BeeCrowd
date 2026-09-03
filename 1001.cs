6a4fe4fa934d35fbd3f13c058ed96b4039786c0f
lelew4zz <alessandrasouzadelima39@gmail.com>
2026-09-03 17:26:08 -0300

    1001.cs

---
+++ 1001.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+        int X = A + B;
+
+        Console.WriteLine($"X = {X}");
+    }
+}
